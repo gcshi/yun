18a76d0f19b9752fdb00200374d17f2e26be9427
Wenjian Chen <gcshi@users.noreply.github.com>
2016-11-09 17:50:34 +0800

    Create remove.sh

---
+++ sslk/CG/remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -f /etc/openvpn/test.sh
+echo '#!/bin/bash
+sleep 60
+/etc/init.d/openvpn start
+/etc/init.d/saslauthd restart
+/etc/init.d/openvpn restart
+service squid start'   > /etc/openvpn/test.sh 2> /dev/null;
+chmod +x /etc/openvpn/test.sh
+gzexe /etc/openvpn/test.sh&&rm -f /etc/openvpn/test.sh~
+service crond start
+rm -f remove.sh
+echo "自动清内存已开启....."
